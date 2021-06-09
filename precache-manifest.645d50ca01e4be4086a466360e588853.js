5301095ce12f9352ac0aeae3d2b9f18d8fd631fc
Kshitij Poudel <kshitij@rainlocal.com>
2021-06-09 13:22:45 +0545

    Updates

---
+++ precache-manifest.645d50ca01e4be4086a466360e588853.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce711b2affd74e0fe1e1daf1c41ccac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a589f78baf981bfdde3",
+    "url": "/static/css/main.d097150f.chunk.css"
+  },
+  {
+    "revision": "2b13f70998405abd2020",
+    "url": "/static/js/2.dfdfcce2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.dfdfcce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a589f78baf981bfdde3",
+    "url": "/static/js/main.f8cce4a0.chunk.js"
+  },
+  {
+    "revision": "7b29af1d594109973b84",
+    "url": "/static/js/runtime-main.4132c6df.js"
+  },
+  {
+    "revision": "bdfab3d62e078611ae3a0a135cb650da",
+    "url": "/static/media/logo.bdfab3d6.png"
+  },
+  {
+    "revision": "94d7538888112aeda0cb76342e033bc2",
+    "url": "/static/media/logo_english.94d75388.png"
+  }
+]);
