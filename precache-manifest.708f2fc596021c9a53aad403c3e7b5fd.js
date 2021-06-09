365dd98d878cda8a3b3e5a4dddb82253009ba7f3
Kshitij Poudel <kshitij@rainlocal.com>
2021-06-09 13:46:27 +0545

    Updates

--- precache-manifest.708f2fc596021c9a53aad403c3e7b5fd.js
+++ precache-manifest.708f2fc596021c9a53aad403c3e7b5fd.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce711b2affd74e0fe1e1daf1c41ccac9",
+    "revision": "e2b29de9644ec0c26de9cbdc71fa4d40",
     "url": "/index.html"
   },
   {
-    "revision": "3a589f78baf981bfdde3",
+    "revision": "0e87bc8d38356221bdb0",
     "url": "/static/css/main.d097150f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.dfdfcce2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3a589f78baf981bfdde3",
-    "url": "/static/js/main.f8cce4a0.chunk.js"
+    "revision": "0e87bc8d38356221bdb0",
+    "url": "/static/js/main.656f3d46.chunk.js"
   },
   {
     "revision": "7b29af1d594109973b84",
